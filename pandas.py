01cf095f4a216bcffa71b70d1bf9151b2932d5c5
Hema Deepthi Karmoory <hema@Hemas-MacBook-Air.local>
2018-12-02 21:11:27 -0600

    added decision tree

--- pandas.py
+++ pandas.py
@@ -8,6 +8,7 @@ Created on Sat Dec  1 23:23:47 2018
 
 import pandas as pd
 import os
+from sklearn import tree
 
 os.getcwd()
 titanic_train = pd.read_csv("/Users/hema/Documents/DS_Learning/git/titanic/train.csv")
@@ -21,3 +22,18 @@ titanic_train.describe()
 titanic_train[2:4]
 titanic_train[0:11]
 titanic_train[0:11]['Fare']
+titanic_train[:]['Embarked']
+titanic_train['Embarked']
+titanic_train[10:11]
+titanic_train[9:10]
+
+x_train = titanic_train[['Pclass','SibSp','Parch']]
+y_train = titanic_train['Survived']
+dt = tree.DecisionTreeClassifier()
+dt.fit(x_train,y_train)
+
+titanic_test = pd.read_csv("/Users/hema/Documents/DS_Learning/git/titanic/test.csv")
+titanic_test['Survived'] = dt.predict(titanic_test[['Pclass','SibSp','Parch']])
+titanic_test.to_csv("/Users/hema/Documents/DS_Learning/git/titanic/prediction1.csv", columns = ['PassengerId','Survived'], index = False)
+
+
